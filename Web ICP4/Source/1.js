8d049fc8a04321b790dd5dd081762ec94a4e6e14
chakradharchinnam <55292378+chakradharchinnam@users.noreply.github.com>
2020-06-25 17:53:30 -0500

    webicp4

--- Web ICP4/Source/1.js
+++ Web ICP4/Source/1.js
@@ -3,20 +3,7 @@ function upDate(previewPic) {
     console.log(previewPic);
     $('#image').css('background-image', 'url(' + previewPic.src + ')');
     $('#image').html(previewPic.alt);
-//     if(previewPic.alt=="Batter is ready")
-// {
-//     $('#image').src="https://cdn.sallysbakingaddiction.com/wp-content/uploads/2017/06/moist-chocolate-cupcakes-7.jpg";
-//     $('#image').html("Batter is ready");
-// }
-//     else if (previewPic.alt=="Perfect Baking")
-//     {
-//         $('#image').src = "https://cdn.sallysbakingaddiction.com/wp-content/uploads/2017/06/moist-chocolate-cupcakes-6.jpg";
-//         $('#image').html("Perfect Baking");
-//     }
-//     else if(previewPic.alt=="Yummy yummy cup cake"){
-//         $('#image').src = "https://cdn.sallysbakingaddiction.com/wp-content/uploads/2017/06/moist-chocolate-cupcakes-5.jpg";
-//         $('#image').html("Yummy yummy cup cake");
-//     }
+
 }
 
 function unDo() {
